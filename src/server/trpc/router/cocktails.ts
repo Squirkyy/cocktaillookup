65c7b4622a898747cf6d9b0869730048b7afb6e5
Darius Vollmer <dariusv@outlook.de>
2023-01-02 13:17:55 +0100

    fixed the cocktails router so it fetches all cocktails now

--- src/server/trpc/router/cocktails.ts
+++ src/server/trpc/router/cocktails.ts
@@ -1,15 +1,18 @@
-import {publicProcedure, router} from "../trpc";
+import { publicProcedure, router } from "../trpc";
+import axios from "axios";
+
+const baseUrl = "http://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
+const letters: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+const fetchData = async (letter: string): Promise<any> => {
+  const response = await axios.get(baseUrl + letter);
+  return response.data;
+}
 
 export const cocktails = router({
     allCocktails: publicProcedure
         .query(async () => {
-            const letters = "abcdefghijklmnopqrstuvwxyz";
-            const lettersArray = [...letters];
-            const data = await Promise.all(lettersArray.map(letter => {
-                fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`).then(resp => resp.json());
-            }))
-            return {
-                cocktails: data,
-            }
+          const request = letters.map(letter => fetchData(letter));
+          return Promise.all(request);
         })
 })
