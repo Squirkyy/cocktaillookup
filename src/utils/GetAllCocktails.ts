9f16c69bd12cb66faa2641d923ceba3a3576ece5
Darius Vollmer <dariusv@outlook.de>
2023-01-02 13:21:43 +0100

    removed helper GetAllCocktails.ts

--- src/utils/GetAllCocktails.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const GetAllCocktails = () => {
-    const letters = "abcdefghijklmnopqrstuvwxyz";
-    const lettersArray = [...letters];
-    lettersArray.forEach(async (letter) => {
-        const { data } = await axios(`www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`);
-    })
-};
